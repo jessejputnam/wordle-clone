42562dace8dde5505f8179dc1e51c53be7cfc627
Jesse Putnam <jessejputnam@gmail.com>
2022-03-17 17:40:38 -0400

    Clicking letters fills chosen box

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// DOM ELEMENTS
-const body = document.querySelector("body");
-const overlay = document.querySelector(".overlay");
-
-const btnOptions = document.querySelector("#options");
-const btnStart = document.querySelector(".btn__start");
-
-const modalOption = document.querySelector(".modal__option");
-
-const checkDarkMode = document.querySelector("#dark-mode");
-
-const mainBoard = document.querySelector(".main");
-
-const gameboard = document.querySelector(".gameboard__container");
-const guessBoxes = document.querySelectorAll(".gameboard__box");
-const guessRows = document.querySelectorAll(".gameboard__row");
-
-const keyboardContainer = document.querySelector(".keyboard__container");
-const keyboardKeys = document.querySelectorAll(".keyboard__key");
-
 // Answer Choices
 const answers = [
   "COULD",
@@ -47,6 +27,26 @@ const answers = [
   "YEARN",
 ];
 
+// DOM ELEMENTS
+const body = document.querySelector("body");
+const overlay = document.querySelector(".overlay");
+
+const btnOptions = document.querySelector("#options");
+const btnStart = document.querySelector(".btn__start");
+
+const modalOption = document.querySelector(".modal__option");
+
+const checkDarkMode = document.querySelector("#dark-mode");
+
+const mainBoard = document.querySelector(".main");
+
+const gameboard = document.querySelector(".gameboard__container");
+const guessRows = document.querySelectorAll(".gameboard__row");
+const guessBoxes = document.querySelectorAll(".gameboard__box");
+
+const keyboardContainer = document.querySelector(".keyboard__container");
+const keyboardKeys = document.querySelectorAll(".keyboard__key");
+
 // Dark Mode Option
 btnOptions.addEventListener("click", function () {
   if (!modalOption.classList.contains("modal--visible")) {
@@ -75,27 +75,48 @@ checkDarkMode.addEventListener("mouseup", function () {
 
 // ------------------------ GAMEPLAY ---------------------------
 let answer;
-let playerAnswer = [];
-let row = 0;
+let playerSubmission;
+let activeRow = 0;
+let playerGuess = "";
 
 // Press button to start game and reveal gameboard
 
 // Randomly choose a correct answer and assign to answer as array of letters
 answer = answers[Math.floor(Math.random() * answers.length)].split("");
 
-// User input for letters (mouse and keyboard)
-// Mouse
+// User input for letters
 keyboardKeys.forEach((key) =>
   key.addEventListener("mousedown", function (e) {
-    console.log(e.target.textContent);
+    // Back deletes last selection
+    if (e.target.textContent === "BACK") {
+      // Delete by creating white space to fill empty guessBoxes
+      playerGuess = playerGuess.slice(0, -1).padEnd(5, " ");
+      return;
+    }
+
+    // Enter ------
+    if (e.target.textContent === "ENTER") return;
+
+    if (playerGuess.length < 5) playerGuess += e.target.textContent;
   })
 );
-// User clicked key logged into guessBox in order
 
-// User can delete previous letter
+// User clicked key logged as  in order
+document.addEventListener("mouseup", function () {
+  // Convert playerGuess to array to be printed to boxes
+  playerSubmission = [...playerGuess];
+
+  //
+  for (let [i, char] of playerSubmission.entries()) {
+    document.querySelector(`.game__row--${activeRow} > .box${i}`).textContent =
+      char;
+  }
+
+  // Remove trim to allow player to add to guess
+  playerGuess = playerGuess.trim();
+});
 
 // User plays word
-// playerAnswer is gathered as array from guessBoxes
 
 // Check each letter of playerAnswer against answer
 // if letter is in word but wrong place -> box turns orange
