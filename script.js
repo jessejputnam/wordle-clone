abf154bb1448c4baff940219318c34b28ee2176f
Jesse Putnam <jessejputnam@gmail.com>
2022-03-18 15:05:44 -0400

    Finish game functionality for play and replay

--- script.js
+++ script.js
@@ -30,11 +30,16 @@ const answers = [
 // DOM ELEMENTS
 const body = document.querySelector("body");
 const overlay = document.querySelector(".overlay");
+const overlay2 = document.querySelector(".overlay2");
 
 const btnOptions = document.querySelector("#options");
 const btnStart = document.querySelector(".btn__start");
 
 const modalOption = document.querySelector(".modal__option");
+const modalGameover = document.querySelector(".modal__gameover");
+const modalGameoverMessage = document.querySelector(".gameover--message");
+const modalGameoverAnswer = document.querySelector(".gameover--answer");
+const btnNewgame = document.querySelector(".btn__newgame");
 
 const checkDarkMode = document.querySelector("#dark-mode");
 
@@ -59,8 +64,10 @@ btnOptions.addEventListener("click", function () {
 });
 
 overlay.addEventListener("click", () => {
-  modalOption.classList.remove("modal--visible");
-  overlay.classList.add("full-hidden");
+  if (modalGameover.classList.contains("full-hidden")) {
+    modalOption.classList.remove("modal--visible");
+    overlay.classList.add("full-hidden");
+  }
 });
 
 checkDarkMode.addEventListener("mouseup", function () {
@@ -74,107 +81,160 @@ checkDarkMode.addEventListener("mouseup", function () {
 });
 
 // ------------------------ GAMEPLAY ---------------------------
-let answer;
-let playerSubmission;
-let activeRow = 0;
-let playerGuess = "";
+// let answer;
+// let playerSubmission;
+// let activeRow = 0;
+// let playerGuess = "";
 
-// Press button to start game and reveal gameboard
+//! Press button to start game and reveal gameboard
 
-// Randomly choose a correct answer and assign to answer as array of letters
-answer = answers[Math.floor(Math.random() * answers.length)].split("");
+const init = () => {
+  let answer;
+  let playerSubmission;
+  let activeRow = 0;
+  let playerGuess = "";
+  // Randomly choose a correct answer and assign to answer as array of letters
+  answer = answers[Math.floor(Math.random() * answers.length)].split("");
+  modalGameoverAnswer.textContent = answer.join("");
 
-// User input for letters
-keyboardKeys.forEach((key) =>
-  key.addEventListener("mousedown", function (e) {
-    // Back deletes last selection
-    if (e.target.textContent === "BACK") {
-      // Delete by creating white space to fill empty guessBoxes
-      playerGuess = playerGuess.slice(0, -1).padEnd(5, " ");
-      return;
-    }
-
-    // If player hits enter too early, prompt to guess more
-    if (e.target.textContent === "ENTER" && playerGuess.length < 5) {
-      const checkRow = document.querySelector(`.game__row--${activeRow}`);
-      checkRow.classList.add("shake");
-      setTimeout(() => checkRow.classList.remove("shake"), 1000);
-      return;
-    }
-
-    // Enter w/ proper length checks against answer
-    if (e.target.textContent === "ENTER") {
-      for (let [i, letter] of playerSubmission.entries()) {
-        // Correct letter, correct place
-        if (letter === answer[i]) {
-          document
-            .querySelector(`.game__row--${activeRow} > .box${i}`)
-            .classList.add("correct-guess");
-
-          keyboardKeys.forEach((key) => {
-            if (key.textContent === letter) {
-              key.classList.remove("partial-guess");
-              key.classList.add("correct-guess");
-            }
-          });
-
-          continue;
-        }
-
-        // Correct letter, wrong place
-        if (answer.includes(letter)) {
-          document
-            .querySelector(`.game__row--${activeRow} > .box${i}`)
-            .classList.add("partial-guess");
-
-          keyboardKeys.forEach((key) => {
-            if (
-              key.textContent === letter &&
-              !key.classList.contains("correct-guess")
-            )
-              key.classList.add("partial-guess");
-          });
-
-          continue;
-        }
-
-        // Wrong letter
-        if (!answer.includes(letter)) {
-          document
-            .querySelector(`.game__row--${activeRow} > .box${i}`)
-            .classList.add("wrong-guess");
-
-          keyboardKeys.forEach((key) => {
-            if (key.textContent === letter) key.classList.add("wrong-guess");
-          });
-
-          continue;
-        }
+  // User input for letters
+  keyboardKeys.forEach((key) =>
+    key.addEventListener("mousedown", function (e) {
+      // Back deletes last selection
+      if (e.target.textContent === "BACK") {
+        // Delete by creating white space to fill empty guessBoxes
+        playerGuess = playerGuess.slice(0, -1).padEnd(5, " ");
+        return;
       }
 
-      activeRow++;
-      playerGuess = "";
-      return;
+      // If player hits enter too early, prompt to guess more
+      if (e.target.textContent === "ENTER" && playerGuess.length < 5) {
+        const checkRow = document.querySelector(`.game__row--${activeRow}`);
+        checkRow.classList.add("shake");
+        setTimeout(() => checkRow.classList.remove("shake"), 1000);
+        return;
+      }
+
+      // Enter w/ proper length checks against answer
+      if (e.target.textContent === "ENTER") {
+        // Lose condition
+        if (activeRow === 5 && playerGuess !== answer.join("")) {
+          modalGameoverMessage.textContent = "Sorry, chump!";
+          modalGameover.classList.remove("full-hidden");
+          overlay2.classList.remove("full-hidden");
+        }
+
+        // Win condition
+        if (playerGuess === answer.join("")) {
+          modalGameoverMessage.textContent = "You win!";
+          modalGameover.classList.remove("full-hidden");
+          overlay2.classList.remove("full-hidden");
+        }
+
+        for (let [i, letter] of playerSubmission.entries()) {
+          // Correct letter, correct place
+          if (letter === answer[i]) {
+            document
+              .querySelector(`.game__row--${activeRow} > .box${i}`)
+              .classList.add("correct-guess");
+
+            keyboardKeys.forEach((key) => {
+              if (key.textContent === letter) {
+                key.classList.remove("partial-guess");
+                key.classList.add("correct-guess");
+              }
+            });
+
+            continue;
+          }
+
+          // Correct letter, wrong place
+          if (answer.includes(letter)) {
+            document
+              .querySelector(`.game__row--${activeRow} > .box${i}`)
+              .classList.add("partial-guess");
+
+            keyboardKeys.forEach((key) => {
+              if (
+                key.textContent === letter &&
+                !key.classList.contains("correct-guess")
+              )
+                key.classList.add("partial-guess");
+            });
+
+            continue;
+          }
+
+          // Wrong letter
+          if (!answer.includes(letter)) {
+            document
+              .querySelector(`.game__row--${activeRow} > .box${i}`)
+              .classList.add("wrong-guess");
+
+            keyboardKeys.forEach((key) => {
+              if (key.textContent === letter) key.classList.add("wrong-guess");
+            });
+
+            continue;
+          }
+        }
+
+        // Move down active row, reset player guess
+        activeRow++;
+        playerGuess = "";
+        return;
+      }
+
+      // Add guess to playerGuess
+      if (playerGuess.length < 5) playerGuess += e.target.textContent;
+    })
+  );
+
+  // User clicked key logged as  in order
+  document.addEventListener("mouseup", function () {
+    // Convert playerGuess to array to be printed to boxes
+    playerSubmission = [...playerGuess];
+
+    // Print to box
+    for (let [i, char] of playerSubmission.entries()) {
+      document.querySelector(
+        `.game__row--${activeRow} > .box${i}`
+      ).textContent = char;
     }
 
-    // Add guess to playerGuess
-    if (playerGuess.length < 5) playerGuess += e.target.textContent;
-  })
-);
+    // Remove trim to allow player to add to guess
+    playerGuess = playerGuess.trim();
+  });
 
-// User clicked key logged as  in order
-document.addEventListener("mouseup", function () {
-  // Convert playerGuess to array to be printed to boxes
-  playerSubmission = [...playerGuess];
+  // User starts a new game
+  btnNewgame.addEventListener("click", function () {
+    playerGuess = "";
+    activeRow = 0;
+    answer = answers[Math.floor(Math.random() * answers.length)].split("");
+    modalGameoverAnswer.textContent = answer.join("");
 
-  //
-  for (let [i, char] of playerSubmission.entries()) {
-    document.querySelector(`.game__row--${activeRow} > .box${i}`).textContent =
-      char;
-  }
+    modalGameover.classList.add("full-hidden");
+    overlay2.classList.add("full-hidden");
 
-  // Remove trim to allow player to add to guess
-  playerGuess = playerGuess.trim();
+    guessBoxes.forEach((box) => {
+      box.textContent = "";
+      box.classList.remove("partial-guess");
+      box.classList.remove("correct-guess");
+      box.classList.remove("wrong-guess");
+    });
+
+    keyboardKeys.forEach((key) => {
+      key.classList.remove("partial-guess");
+      key.classList.remove("wrong-guess");
+      key.classList.remove("correct-guess");
+    });
+  });
+};
+
+btnStart.addEventListener("click", function () {
+  btnStart.classList.add("full-hidden");
+  mainBoard.classList.remove("hidden");
+  init();
 });
 
 // User plays word
